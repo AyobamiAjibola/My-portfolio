fe0fbc9cfff0ae3856cb88c4cc937364fe1499e7
ayobami ajibola <ayobamiajibola46@gmail.com>
2023-03-26 13:20:38 +0100

    added new project

--- src/components/pages/project/resource.tsx
+++ src/components/pages/project/resource.tsx
@@ -2,6 +2,18 @@ export const data = [
     {
         id: 1,
         project_title: "jiffix-autohyve",
+        work_done: "This project handles workshop backend functionality, allowing workshop users to manage estimates, invoices, and expenses, as well as view monthly analytics. I worked on the expenses functionality, which allows workshop users to generate expenses and track payments, as well as view and edit expense details. This project required me to work on both the backend and the frontend. I modified some of the backend components and APIs, as well as the frontend expenses component; additionally, part of my responsibilities is to make the entire product mobile responsive. The technologies used includes, Nodejs, Reactjs, Typescript, Mongodb, Sequelize, Redis, JOI, JWT, Redux, Material UI",
+        desc: "Jiffix autohyve is a robust operating system (OS) for the automotive after-sales sector in Africa. The goal of jiffix autohyve is Creating Infrastructures that deliver professional and quality automotive services, and the technology for full life-cycle management of all vehicles’ inspections, maintenance & repairs. jiffix autohyve connect each vehicle to the cloud, with tools and technology that ensures safe, reliable and unhindered mobility for all.",
+        repo: "https://github.com/Jiffix-Technologies/AutoHyve-pdms-gateway.git",
+        status: "done",
+        website: "http://18.222.133.239:8083/",
+        img: "assets/jiffix-gateway1.jpg",
+        img2: "assets/jiffix-gateway2.jpg",
+        role: "Full Stack Developer"
+    },
+    {
+        id: 2,
+        project_title: "jiffix-autohyve",
         work_done: "The technologies used includes, Nodejs, Reactjs, typescript, Mongodb, Sequelize, Redis, JOI, JWT, Redux, Material UI. I'm in charge of integrating the apis into the Jiffix web application. I changed a few of the backend parts and apis, and I also integrated the apis into the web application. This part of the project took one month to be completed. I'm part of a team of 4, and I'm a full stack developer on the team.",
         desc: "Jiffix autohyve is a robust operating system (OS) for the automotive after-sales sector in Africa. The goal of jiffix autohyve is Creating Infrastructures that deliver professional and quality automotive services, and the technology for full life-cycle management of all vehicles’ inspections, maintenance & repairs. jiffix autohyve connect each vehicle to the cloud, with tools and technology that ensures safe, reliable and unhindered mobility for all.",
         repo: "https://github.com/Jiffix-Technologies/AutoHyve-web-app.git",
@@ -12,7 +24,7 @@ export const data = [
         role: "Full Stack Developer"
     },
     {
-        id: 2,
+        id: 3,
         project_title: "esun ekiti",
         work_done: "The technologies used to develop this project include  HTML, CSS, React.js for the client and Node.js and express, JWT, Sequelize for the server. Typescript is used to write the entire codebase.",
         desc: "Esun Ekiti is a small town in Nigeria. This project's objectives include spreading awareness of Esun's rich history and culture as well as monitoring and informing community development initiatives. additionally, to educate the public about the activities and events taking place in Esun Ekiti.",
@@ -24,7 +36,7 @@ export const data = [
         role: "Full Stack Developer"
     },
     {
-        id: 3,
+        id: 4,
         project_title: "kweekbuy",
         work_done: "I handled the client side code base for this project using HTML, CSS, Bootstrap, and JavaScript to create a responsive user interface while working in a team of three developers.",
         desc: "The need for vendors to connect with more customers who require their goods, services, and products is ongoing and expanding. Additionally, buyers who require these products should be able to contact and connect with vendors who provide the services they require with just a few clicks from their devices. In order to meet these needs, we want to build a platform that will benefit both the vendor and the buyer. Kweekbuy is a platform that enables buyers and sellers to complete their transactions in good time. This project's primary goal is to close the gap between consumers and local vendors. Customers can perform a product search and have the results filtered by the vendors closest to them.",
@@ -36,7 +48,7 @@ export const data = [
         role: "Frontend Developer"
     },
     {
-        id: 4,
+        id: 5,
         project_title: "movote",
         work_done: "I used a number of technologies in the development of this application. The client-side stacks includes HTML, CSS, React, MUI, and context API for state management. The backend stack consists of Node and Express, JWT, and MongoDB. Typescript was used to write the entire client and server-side code.",
         desc: "Movote is a voting platform for organizations that want to integrate technology into their voting process. We believe accountability and transparency are critical components of the voting process, and that is what this project seeks to address. Movote is a web application that will facilitate voting and eliminate any form of vote manipulation by voters, contestants, or administrators. This project handles two types of voting: those that require a single winner for a contest and those that allow for multiple winners from different categories.",
@@ -48,7 +60,7 @@ export const data = [
         role: "Full Stack Developer"
     },
     {
-        id: 5,
+        id: 6,
         project_title: "My Portfolio",
         work_done: "This is a react static project hosted of vercel. No backend codes, just react, scss, and mui",
         desc: "My Portfolio",
@@ -60,7 +72,7 @@ export const data = [
         role: "Frontend Developer"
     },
     {
-        id: 6,
+        id: 7,
         project_title: "registration-form",
         work_done: "For this project, I developed the backend and frontend logic codes and UI. I utilized NodeJS and Express for the server side, ReactJS and MUI for the frontend, JWT for authorization and PSQL for the database in the backend. A also used nodemailer in the server side logic to automatically send mail after filling and submitting the form.",
         desc: "This project is an online form for training centers. It has a backend admin portal where admin can manage the forms sent and also use the information to track payment",
@@ -72,7 +84,7 @@ export const data = [
         role: "Full Stack Developer"
     },
     {
-        id: 7,
+        id: 8,
         project_title: "rimaplay",
         work_done: "The technologies used for the client side of this project are, HTML, CSS, React, and JavaScript. While for the backend, the stacks are Node.js and Express, MongoDb. I handled the AWS part of the project and also some components in the server side and client side ",
         desc: "Providing a one stop shop for everything entertainment and information is the sole purpose of this project. We aim at providing users with a cheaper and more reliable way of having access to constant information and excitement.Rimaplay is centered around creating a hub of information. We believe everyone is entitled to knowledge, as that is important to the growth of individuals. We want to create a system that enables users to have access to valuable information as well as entertainment with just one portal Our approach is to give one access to movies as well as a live stream with an introduction of live or saved news to keep everyone informed of what goes on around them and create more awareness within the society.",
@@ -84,7 +96,7 @@ export const data = [
         role: "Full Stack Developer"
     },
     {
-        id: 8,
+        id: 9,
         project_title: "PO-Smart-App",
         work_done: "I am part of the development team as volunteer, we are responsible for the backend development of this project. The stack we are using includes NodeJS and Express for the server side, Typescript, JOI for server-side validation, MongoDB for the database, and JWT for authentication and authorization.",
         desc: "This solution validates voting results posted by INEC officials against the results posted by LP party agents. It is simply an app for the delegates to report the live happenings of the election in real time for transparency's sake.",
@@ -96,7 +108,7 @@ export const data = [
         role: "Backend Developer"
     },
     {
-        id: 9,
+        id: 10,
         project_title: "restaurant-POS-system",
         work_done: "I was a member of the development team that created this offline point of sale system for restaurants. The frontend and database structure were my main contributions to this project. HTML and CSS were used for the frontend, PHP for the backend logic, and Mysql for the database.",
         desc: "This is a restaurant POS system that helps with sales, inventory management, and finances management. It was deployed in GEM House, a restaurant in Wuse 2 Abuja, Nigeria.",
@@ -108,7 +120,7 @@ export const data = [
         role: "Frontend Developer"
     },
     {
-        id: 10,
+        id: 11,
         project_title: "Gadget-hub-blog",
         work_done: "To make this project a success, some technologies will be used to develop this solution. The server side uses NodeJS, Sequelize, PostgreSQL, REST, JWT, and HTML, CSS, JavaScript, React, MUI, and Context Api for the frontend. The entire codebase will be written in Typescript",
         desc: "Gadgets (such as phones, tablets, laptops, etc.) are progressively ingrained in our daily lives. To know which gadget is hip and what features it has, one needs to do some research before buying or owning one. Knowing where to look for these comparisons is one of the significant issues users encounter; as a result, they frequently settle for the least expensive option rather than the best. This project offers a solution to this issue; GHB is a web-based application that allows users to compare two electronic devices side by side by providing a detailed view of each device's ram, processor, storage, camera, and other specifications. GHB also updates users on recent developments in technology.",
@@ -120,7 +132,7 @@ export const data = [
         role: "Full Stack Developer"
     },
     {
-        id: 11,
+        id: 12,
         project_title: "Foodo",
         work_done: "I'm a lead developer on the project's development team, and part of my responsibilities is to create the models, apis, and backend code. Along with overseeing the development of the mobile apps, I am also in charge of the web apps. The stack we are using for this project includes, Typescript, Node and express for server side, Joi for validation, Sequelize PSQL for the database, JWT for authentication and authorization, ReactJS and MUI for UI, ContextApi for state management and flutter for the mobile apps",
         desc: "Foodo was born out of the customer desire for a faster and more efficient form of food delivery. Customers around the world have 3 major pain points as touching food delivery: ❖Order duration, ❖High Cost of delivery, ❖Quality of delivered food.In order to reduce delivery delays, the project algorithm will be built to match users with restaurants and riders closest to them. Order duration can be a major pain for users. To ensure that the food the vendors deliver is of a high standard, there will be procedures for them to follow and requirements they must fulfill to become a vendor. The goal of this project is to build an automated food delivery system which prioritize on speed, quality and customer service. Every thought process, design and code are aimed at achieving this goal.",
